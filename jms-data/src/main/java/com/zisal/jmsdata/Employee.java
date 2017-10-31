00f23d5d15b4862dcb1a8407c72419f62660f5ac
KNIGHTMASTER <fauzi.knightmaster.achmad@gmail.com>
2017-10-31 23:21:52 +0700

    Added shared Data Project for Transport Object

---
+++ jms-data/src/main/java/com/zisal/jmsdata/Employee.java
@@ -0,0 +1,38 @@
+package com.zisal.jmsdata;
+
+import java.io.Serializable;
+
+/**
+ * Created on 10/12/17.
+ *
+ * @author <a href="mailto:fauzi.knightmaster.achmad@gmail.com">Achmad Fauzi</a>
+ */
+public class Employee implements Serializable {
+
+    private String name;
+    private String address;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
+}
